e9687f6ad734f55eb5e21b1039fab4b788d4540e
Robertfnicholson <93954896+Robertfnicholson@users.noreply.github.com>
2021-12-04 09:27:39 -0500

    Upload Pypoll latest
    Changed the name from Pypoll_2 to PyPoll

--- PyPoll.py
+++ PyPoll.py
@@ -4,23 +4,28 @@
 #3. The percentage of votes each candidate won
 #4. The total number of votes each candidate won
 #5. The winner of the election based on the popular vote
+# Add our dependencies
 import csv
 import os
 # Assign a variable for the file to load and the path.
-file_to_load = 'C:/Users/RFNichol/election_results.csv'
-# Open the election results and read the file
+file_to_load = os.path.join('/Users/RFNichol', 'election_results.csv')
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join('/Users/RFNichol/analysis', 'election_analysis.txt')
+
+# 1. Initialize a total vote counter.
+total_votes = 0
+
+# Open the election results and read the file.
 with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
 
-     # To do: perform analysis.
-     print(election_data)
+    # Read the header row.
+    headers = next(file_reader)
 
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("C:/Users/RFNichol/analysis", "election_analysis.txt")
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # 2. Add to the total vote count.
+        total_votes += 1
 
-# Use the open statement to open the file as a text file.
-with open(file_to_save, "w") as txt_file:
-     # Write some data to the file.
-     txt_file.write("Hello World")
-
-     # Close the file
-     txt_file.close()
+# 3. Print the total votes.
+print(total_votes)
