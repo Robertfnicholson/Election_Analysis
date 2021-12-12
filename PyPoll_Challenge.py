92b107ad3d829402f1383aed94b6d1757a82e988
Robertfnicholson <93954896+Robertfnicholson@users.noreply.github.com>
2021-12-12 14:47:51 -0500

    Updated PyPoll Challenge file

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -6,9 +6,9 @@ import csv
 import os
 
 # Add a variable to load a file from a path.
-file_to_load = os.path.join('/Users/RFNichol', 'election_results.csv')
+file_to_load = os.path.join("Resources", "election_results.csv")
 # Add a variable to save the file to a path.
-file_to_save = os.path.join('/Users/RFNichol/analysis', 'election_analysis.txt')
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Initialize a total vote counter.
 total_votes = 0
